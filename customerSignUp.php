ff04b3a0aaebfb915339fd3c808d465fd271a8aa
Alison <alison.kipyegon@strathmore.edu>
2024-06-20 16:30:20 +0300

    updated CSS for registration and login pages

--- customerSignUp.php
+++ customerSignUp.php
@@ -90,32 +90,39 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
-<!-- Main CSS File -->
-<link rel="stylesheet" href="style.css">
+<!-- Boxicon CSS -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<!-- Sign Up CSS File -->
+<link rel="stylesheet" href="signup.css">
     <title>Customer Sign Up</title>
 </head>
 <body>
-    <section id="home">
-        <div class="home-container">
-            <div id="homeCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
-      
-              <ol class="carousel-indicators" id="home-carousel-indicators"></ol>
-
-                <div class="carousel-inner" role="listbox">
-
-                    <h1>Sign Up</h1>
+    <div class="sign-up">
+        <div class="wrapper">
+                <h1>Sign Up</h1>
                     <form action="" method="post" enctype="multipart/form-data">
-                        <input type="text" name="custName" placeholder="Name" required><br><br>
-                        <input type="email" name="email" placeholder="Email" required><br><br>
-                        <input type="number" name="phoneNo" placeholder="Phone No." required><br><br>
-                        <input type="password" name="password" placeholder="Enter Password" required><br><br>
-                        <input type="password" name="confpassword" placeholder="Confirm Password" required><br><br>
-                        <input type="submit" name="submit" placeholder="Sign Up"><br><br>
+                        <div class="input-box">
+                            <input type="text" name="custName" placeholder="Name" required>
+                        </div>
+                        <div class="input-box">
+                            <input type="email" name="email" placeholder="Email" required>
+                        </div>
+                        <div class="input-box">
+                            <input type="number" name="phoneNo" placeholder="Phone No." required>
+                        </div>
+                        <div class="input-box">
+                             <input type="password" name="password" placeholder="Enter Password" required>
+                        </div>
+                       <div class="input-box">
+                            <input type="password" name="confpassword" placeholder="Confirm Password" required>
+                       </div>
+                        
+                       <button type="submit" class="btn">Register</button>
                     </form>
 
-                </div>
-            </div>
         </div>
-    </section>
+    </div>
+                
+              
 </body>
 </html>
--- restaurantSignUp.php
+++ restaurantSignUp.php
@@ -88,16 +88,16 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
-<!-- Main CSS File -->
-<link rel="stylesheet" href="style.css">
+<!-- Boxicon CSS -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<!-- Sign Up CSS File -->
+<link rel="stylesheet" href="signup.css">
     <title>Restaurant Sign Up</title>
 </head>
 <body>
-
-    <section id="home"> 
-        <div class="home-container">
-            <div id="homeCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                <div class="alert">
+    <div class="sign-up">
+        <div class="wrapper">
+            <div class="alert">
                     <?php
                         if(isset($_SESSION['status']))
                         {
@@ -105,24 +105,43 @@
                             unset($_SESSION['status']);
                         }
                     ?>
-                </div>
-                     <ol class="carousel-indicators" id="home-carousel-indicators"></ol>
-                        <div class="carousel-inner" role="listbox">
-                             <h1>Sign Up</h1>
-                                <form action="" method="post" enctype="multipart/form-data">
-                                    <input type="text" name="restaurantName" placeholder="Restaurant Name" required><br><br>
-                                    <input type="email" name="email" placeholder="Email" required><br><br>
-                                    <input type="location" name="location" placeholder="Location" required><br><br>
-                                    <input type="number" name="phoneNo" placeholder="Phone Number" required><br><br>
-                                    <input type="file" id="file" name="menu" required><br><br>
-                                    <input type="password" name="password" placeholder="password"><br><br>
-                                    <input type="submit" name="submit" placeholder="Sign Up"><br><br>
-                                </form>
-                </div>
-
             </div>
+                     
+             <h1>Sign Up</h1>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="input-box">
+                        <input type="text" name="restaurantName" placeholder="Restaurant Name" required>
+                        <i class='bx bx-restaurant'></i>
+                    </div>
+                    <div class="input-box">
+                        <input type="email" name="email" placeholder="Email" required>
+                        <i class='bx bx-envelope'></i>
+                    </div>
+                    <div class="input-box">
+                        <input type="location" name="location" placeholder="Location" required>
+                        <i class='bx bx-current-location'></i>
+                    </div>
+                    <div class="input-box">
+                         <input type="number" name="phoneNo" placeholder="Phone Number" required>
+                         <i class='bx bx-phone'></i>
+                    </div>                   
+                    <div class="input-box">
+                        <!-- <label for="menu">Upload Menu</label> -->
+                        <i class='bx bx-food-menu'></i>
+                        <input type="file" id="file" name="menu" placeholder="Insert Menu"required>                        
+                    </div>    
+                    <div class="input-box">
+                        <input type="password" name="password" placeholder="password">
+                        <i class='bx bxs-lock-alt'></i>
+                    </div>
+                    <button type="submit" class="btn">Register</button>
+                </form>
+        
         </div>
-    </section>
+    </div>
+    
+    
+             
 
 </body>
 </html>
